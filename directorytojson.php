7587e97f76601cdf9bd938c6a69c67dd440a072f
Nick Giacobe <nxg13@ist.psu.edu>
2017-03-02 13:51:25 -0500

    directorytojson.php
    a simple script to pull the geotags out of jpg files in a directory and publish a GeoJSON file of all of them.

---
+++ directorytojson.php
@@ -0,0 +1,74 @@
+<?php
+function getGps($exifCoord, $hemi) {
+
+    $degrees = count($exifCoord) > 0 ? gps2Num($exifCoord[0]) : 0;
+    $minutes = count($exifCoord) > 1 ? gps2Num($exifCoord[1]) : 0;
+    $seconds = count($exifCoord) > 2 ? gps2Num($exifCoord[2]) : 0;
+
+    $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
+
+    return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+
+}
+
+function gps2Num($coordPart) {
+
+    $parts = explode('/', $coordPart);
+
+    if (count($parts) <= 0)
+        return 0;
+
+    if (count($parts) == 1)
+        return $parts[0];
+
+    return floatval($parts[0]) / floatval($parts[1]);
+}
+
+function reportGps ($exif) {
+
+	$lon = getGps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
+	$lat = getGps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
+	$datetime = $exif["DateTimeOriginal"];
+
+	return array ($lat,$lon,$datetime);
+}
+
+
+// Main
+
+$geojson = array(
+	'type'	=> 'FeatureCollection',
+	'features' =>	array()
+);
+
+$directory = './';
+$scanned_directory = array_diff(scandir($directory), array('..', '.', '.images.php.swp', 'images.php','.php'));
+
+
+
+foreach ($scanned_directory as $filename) {
+	
+	if (strpos($filename, '.jpg') !== false ) {
+		$exifforfile = exif_read_data($filename);
+		list ($lat,$lon,$datetime) = reportGps ($exifforfile);
+
+		$feature = array(
+			'type' => 'Feature',
+			'properties' => array (
+				'image' => 'http://rcal.ist.psu.edu/stadium/'.$filename,
+				'name' => 'http://rcal.ist.psu.edu/stadium/'.$filename
+			),
+			'geometry' => array (
+				'type' => 'Point',
+				'coordinates' => (array($lon,$lat)
+			)
+		));
+		array_push($geojson['features'],$feature);
+
+	}
+}
+header('Content-type: application/json');
+echo json_encode($geojson, JSON_NUMERIC_CHECK);
+
+?>
+
